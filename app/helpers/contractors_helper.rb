edac2c680a20d87606907b410655dff7eb0c1826
John Trichereau <johntrichereau@gmail.com>
2014-10-13 14:08:45 +0200

    Set up devise / Users / Contractors with shared and distinct views

---
+++ app/helpers/contractors_helper.rb
@@ -0,0 +1,13 @@
+module ContractorsHelper
+	# def resource_name
+ #    :contractor
+ #  end
+
+ #  def resource
+ #    @resource ||= Contractor.new
+ #  end
+
+ #  def devise_mapping
+ #    @devise_mapping ||= Devise.mappings[:contractor]
+ #  end
+end
