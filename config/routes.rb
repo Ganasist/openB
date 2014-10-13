c6c15e5df664a5a2b4125f2e0209eddf9be680cb
John Trichereau <johntrichereau@gmail.com>
2014-10-13 13:03:17 +0200

    Update routes for U/C

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,29 @@
 Rails.application.routes.draw do
-  devise_for :contractors
-  mount Upmin::Engine => '/admin'
+
+
+	devise_for :users, controllers: { 
+									 					confirmations: 'users/confirmations',
+										 	 # omniauth_callbacks: 'users/omniauth_callbacks',
+											  			  passwords: 'users/passwords',
+													  registrations: 'users/registrations',
+												 			 	 sessions: 'users/sessions',
+												 				  unlocks: 'users/unlocks' }
+
+	devise_for :contractors, controllers: { 
+														confirmations: 'contractors/confirmations',
+											 # omniauth_callbacks: 'contractors/omniauth_callbacks',
+																passwords: 'contractors/passwords',
+														registrations: 'contractors/registrations',
+											 					 sessions: 'contractors/sessions',
+											 					  unlocks: 'contractors/unlocks' }
+
+  # devise_for :contractors
+  # devise_for :users
   
-  devise_for :users
   resources :users
 
   resource :contact, only: [:new, :create]
 
+  mount Upmin::Engine => '/admin'
   root :to => 'high_voltage/pages#show', id: 'splash'
 end
