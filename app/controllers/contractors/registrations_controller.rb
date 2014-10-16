c198bdc01a3dc5cfad63eee37ccb209411183614
John Trichereau <johntrichereau@gmail.com>
2014-10-16 17:18:38 +0200

    MU

--- app/controllers/contractors/registrations_controller.rb
+++ app/controllers/contractors/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Contractors::RegistrationsController < Devise::RegistrationsController
   # before_filter :configure_sign_up_params, only: [:create]
   before_filter :configure_account_update_params, only: [:update]
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  # before_filter :configure_permitted_parameters, if: :devise_controller?
 
   # GET /resource/sign_up
   def new
