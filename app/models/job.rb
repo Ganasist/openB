d35fea38b864924664731e5c63338a0f89bf4b15
John Trichereau <johntrichereau@gmail.com>
2014-10-21 14:53:32 +0200

    Add edit link to job show view, change edit HTML to match Show

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,4 +8,5 @@ class Job < ActiveRecord::Base
 
   validates :title, presence: true, allow_blank: false, length: { in: 5..50 }
   validates :description, presence: true, allow_blank: false, length: { in: 10..500 }
+  validates :categories, length: { minimum: 1, maximum: 3, message: 'Pick between 1 to 3 categories' }, allow_blank: false
 end
