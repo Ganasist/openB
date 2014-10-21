73abcdcb1aeb95c45ac99d0b3b814ebd9b438163
John Trichereau <johntrichereau@gmail.com>
2014-10-21 19:45:44 +0200

    Update strong params for Jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -63,7 +63,8 @@ class JobsController < ApplicationController
     end
 
     def job_params
-      params.require(:job).permit(:zip_code, :phone, :title, 
+      params.require(:job).permit(:zip_code, :phone, :title, :cost,
+                                  :duration, :duration_unit,
                                   :description, :image, :delete_image,
                                   :image_remote_url, { categories: [] })
     end
