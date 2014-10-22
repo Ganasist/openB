bd76c338046b69405ca6eb9d4d4694d0036ac01f
John Trichereau <johntrichereau@gmail.com>
2014-10-22 11:56:03 +0200

    Update Job show page and add validations to duration, cost and duration_unit

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,6 +6,11 @@ class Job < ActiveRecord::Base
   belongs_to :user
   belongs_to :contractor
 
+  DURATION_UNITS = ['minutes', 'hours', 'days', 'weeks', 'months']
+
+  validates :cost, :duration, :duration_unit, presence: true
+  validates :cost, :duration, numericality: true
+  validates :duration_unit, inclusion: { in: DURATION_UNITS }
   validates :title, presence: true, allow_blank: false, length: { in: 5..50 }
   validates :description, presence: true, allow_blank: false, length: { in: 10..500 }
   validates :categories, presence: true, length: { maximum: 3, message: 'Pick between 1-3 categories' }
@@ -16,8 +21,11 @@ class Job < ActiveRecord::Base
   	self.zip_code = self.user.zip_code
   end
 
-  def self.duration_units
-    ['minutes', 'hours', 'days', 'weeks', 'months']
+  def job_duration
+		"#{ self.duration } #{ self.duration_unit }"  	
   end
 
+  def job_cost
+  	"$#{ self.cost }"
+  end
 end
