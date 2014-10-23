08370bfeabe81c4ec16d2607e789887990719e15
John Trichereau <johntrichereau@gmail.com>
2014-10-23 12:21:19 +0200

    Update bid form, preliminary Bid posting done

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,7 +1,28 @@
 class BidsController < ApplicationController
-
+	before_filter :authenticate_contractor!
 	def new
 		@job = Job.find(params[:job_id])
-		@bid = @job.bids.build(job: @job, contractor: current_contractor)
+		@bid = @job.bids.new
 	end
+
+	def create
+    @bid = Bid.new(bid_params)
+    @bid.contractor = current_contractor
+    @bid.job = Job.find(params[:job_id])
+    if @bid.save
+      redirect_to current_contractor, notice: 'Bid was successfully created!'
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def set_bid
+      @bid = Bid.find(params[:id])
+    end
+
+    def bid_params
+      params.require(:bid).permit(:cost, :duration, :duration_unit)
+    end
+
 end
