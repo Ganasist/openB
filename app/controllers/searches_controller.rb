a86ebc8dc15ae483d7d26602aa76ae5055d5219c
John Trichereau <johntrichereau@gmail.com>
2014-10-24 15:25:49 +0200

    More search updates

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,8 @@
 class SearchesController < ApplicationController
 	def show
 		if params[:search]
-			@jobs = Job.near(params[:search], 100).order(:updated_at).reverse_order
-			@contractors = Contractor.near(params[:search], 100).order(:updated_at).reverse_order
+			@jobs = Job.near(params[:search], 50).order(:updated_at).reverse_order
+			@contractors = Contractor.near(params[:search], 50).order(:updated_at).reverse_order
 		end
 	end
 end
