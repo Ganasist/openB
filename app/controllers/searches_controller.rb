ae80cf60f3f2444d4f39242292a146dc023c18ab
John Trichereau <johntrichereau@gmail.com>
2014-10-24 18:20:55 +0200

    Update search form and iMac5K image

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,11 @@
 class SearchesController < ApplicationController
 	def show
-		if params[:search]
+		if params[:search].present?
 			@jobs = Job.near(params[:search], 50).order(:updated_at).reverse_order
 			@contractors = Contractor.near(params[:search], 50).order(:updated_at).reverse_order
+		else
+			@jobs = Job.all.limit(20)
+			@contractors = Contractor.all.limit(20)
 		end
 	end
 end
