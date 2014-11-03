2f290ff69510bd07a9042b95ed5f1d4d54ef499d
John Trichereau <johntrichereau@gmail.com>
2014-11-03 21:10:46 +0100

    More updates to Examples

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,7 +1,53 @@
 class ExamplesController < ApplicationController
+	before_action :set_example, only: [:show, :edit, :update, :destroy]
 
 	def new
 		@contractor = Contractor.find(params[:contractor_id])
 		@example = @contractor.examples.build	
 	end
+
+	def show
+		
+	end
+
+	def create
+    @example = Example.new(example_params)
+    # @example.contractor = current_contractor
+    if @example.save
+      flash[:notice] = 'Example was successfully created.'
+      redirect_to @example
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @example = example.find(params[:id])
+    if @example.update(example_params)
+      flash[:notice] = 'example was successfully updated.'
+      redirect_to :back, notice: "Your example for '#{ @example.job.title }' has been updated"
+    else
+      redirect_to :back, alert: "#{ @example.errors.full_messages.to_sentence }"
+    end
+  end
+
+  def destroy
+    @example = example.find(params[:id])
+    @job = @example.job
+    @example.destroy
+    redirect_to current_contractor, notice: "Your example for '#{ @job.title }' has been removed"
+  end
+
+  private
+  	def set_example
+  		@example = Example.find(params[:id])
+  	end
+
+  	def example_params
+  		params.require(:example).permit(:contractor, :title, :zip_code, 
+  																		:description, :duration, :duration_unit,
+  																		:cost, { categories: []}, 
+  																		:image_before, :delete_image_before, :image_before_remote_url,
+																			:image_after, :delete_image_after, :image_after_remote_url )
+  	end
 end
--- app/models/example.rb
+++ app/models/example.rb
@@ -1,41 +1,43 @@
 class Example < ActiveRecord::Base
 
-	attr_accessor :delete_before_image
-  attr_reader :before_image_remote_url  
+  DURATION_UNITS = ['hours', 'days', 'weeks', 'months']
+
+	attr_accessor :delete_image_before
+  attr_reader :image_before_remote_url  
   
-  before_validation { before_image.clear if delete_before_image == '1' }
+  before_validation { image_before.clear if delete_image_before == '1' }
   
-  has_attached_file :before_image, styles: { thumb: '100x100>',
+  has_attached_file :image_before, styles: { thumb: '100x100>',
 																  	 medium: '300x300>',
   																 original: '800x800>' },
 											 default_url: 'jobs-default-:style.png'                 
   
-  validates_attachment :before_image, size: { in: 0..3.megabytes, 
+  validates_attachment :image_before, size: { in: 0..3.megabytes, 
   																			 message: 'Picture must be less than 3 megabytes' }
   
-  validates_attachment_content_type :before_image, content_type: /^before_image\/(png|gif|jpeg|jpg)/,
-		                                     				 message: 'only (png/gif/jpeg) before_images'
+  validates_attachment_content_type :image_before, content_type: /^image\/(png|gif|jpeg|jpg)/,
+		                                     				 message: 'only (png/gif/jpeg) image_befores'
 
-	process_in_background :before_image, processing_image_url: 'ajax_spinner.gif'
+	process_in_background :image_before, processing_image_url: 'ajax_spinner.gif'
 
 
-	attr_accessor :delete_after_image
-  attr_reader :after_image_remote_url  
+	attr_accessor :delete_image_after
+  attr_reader :image_after_remote_url  
   
-  before_validation { after_image.clear if delete_after_image == '1' }
+  before_validation { image_after.clear if delete_image_after == '1' }
   
-  has_attached_file :after_image, styles: { thumb: '100x100>',
+  has_attached_file :image_after, styles: { thumb: '100x100>',
       																  	 medium: '300x300>',
         																 original: '800x800>' },
               											  default_url: 'jobs-default-:style.png'                 
   
-  validates_attachment :after_image, size: { in: 0..3.megabytes, 
+  validates_attachment :image_after, size: { in: 0..3.megabytes, 
   																			message: 'Picture must be less than 3 megabytes' }
   
-  validates_attachment_content_type :after_image, content_type: /^after_image\/(png|gif|jpeg|jpg)/,
-		                                     				 message: 'only (png/gif/jpeg) after_images'
+  validates_attachment_content_type :image_after, content_type: /^image\/(png|gif|jpeg|jpg)/,
+		                                     				 message: 'only (png/gif/jpeg) image_afters'
 
-	# process_in_background :after_image, processing_image_url: 'ajax_spinner.gif'
+	# process_in_background :image_after, processing_image_url: 'ajax_spinner.gif'
   
 
 	def before_image_remote_url=(url_value)
