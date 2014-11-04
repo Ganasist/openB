d851564edc10c574a3e503c605633ca25db1b789
John Trichereau <johntrichereau@gmail.com>
2014-11-04 12:43:34 +0100

    MU

--- db/migrate/20141015133930_add_category_to_models.rb
+++ db/migrate/20141015133930_add_category_to_models.rb
@@ -1,6 +1,6 @@
 class AddCategoryToModels < ActiveRecord::Migration
   def change
     add_column :contractors, :categories, :string, array: true, default: []
-    add_column :posts, :categories, :string, array: true, default: []
+    # add_column :posts, :categories, :string, array: true, default: []
   end
 end
