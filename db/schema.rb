2c419ff1e028ca808d26ef15ed636ee1fc2e4683
John Trichereau <johntrichereau@gmail.com>
2014-11-11 18:38:32 +0100

    Remove acts-as-messagable, incompatible

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111173043) do
+ActiveRecord::Schema.define(version: 20141111142144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,26 +122,6 @@ ActiveRecord::Schema.define(version: 20141111173043) do
   add_index "jobs", ["latitude"], name: "index_jobs_on_latitude", using: :btree
   add_index "jobs", ["longitude"], name: "index_jobs_on_longitude", using: :btree
   add_index "jobs", ["user_id"], name: "index_jobs_on_user_id", using: :btree
-
-  create_table "messages", force: true do |t|
-    t.string   "topic"
-    t.text     "body"
-    t.integer  "received_messageable_id"
-    t.string   "received_messageable_type"
-    t.integer  "sent_messageable_id"
-    t.string   "sent_messageable_type"
-    t.boolean  "opened",                     default: false
-    t.boolean  "recipient_delete",           default: false
-    t.boolean  "sender_delete",              default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "ancestry"
-    t.boolean  "recipient_permanent_delete", default: false
-    t.boolean  "sender_permanent_delete",    default: false
-  end
-
-  add_index "messages", ["ancestry"], name: "index_messages_on_ancestry", using: :btree
-  add_index "messages", ["sent_messageable_id", "received_messageable_id"], name: "acts_as_messageable_ids", using: :btree
 
   create_table "uploads", force: true do |t|
     t.datetime "created_at",                         null: false
