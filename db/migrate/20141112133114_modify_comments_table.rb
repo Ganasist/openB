d161d2fad0d80421a4155686af658a09fb93c277
John Trichereau <johntrichereau@gmail.com>
2014-11-12 16:58:42 +0100

    Remove a migration

--- db/migrate/20141112133114_modify_comments_table.rb
+++
@@ -1,6 +0,0 @@
-class ModifyCommentsTable < ActiveRecord::Migration
-  def change
-  	remove_column :comments, :user_id, :integer, null: false
-  	remove_column :comments, :parent_id, :integer
-  end
-end
