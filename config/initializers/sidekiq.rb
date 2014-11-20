def84921e960427ed731d3faf7b6dbebe9770c03
John Trichereau <johntrichereau@gmail.com>
2014-11-20 14:19:59 +0100

    Readd autoscaler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,26 @@
 require 'sidekiq'
 require 'sidekiq/web'
+require 'autoscaler/sidekiq'
+require 'autoscaler/heroku_scaler'
+
+heroku = nil
+if ENV['HEROKU_APP']
+  heroku = Autoscaler::HerokuScaler.new
+end
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == [ENV['SIDEKIQ_WEB_NAME'], ENV['SIDEKIQ_WEB_PASS']]
 end
 
 Sidekiq.configure_server do |config|
+  config.server_middleware do |chain|
+    if heroku
+      p "[Sidekiq] Running on Heroku, autoscaler is used"
+	    chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuScaler.new, 60) # 60 second timeout
+    else
+      p "[Sidekiq] Running locally, so autoscaler isn't used"
+    end
+  end
   config.redis = { url: ENV['REDISTOGO_URL'],
   								size: 5, 
   					 namespace: "openbid_#{ Rails.env }" }
@@ -13,6 +28,11 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
+	if heroku
+    config.client_middleware do |chain|
+	    chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuScaler.new
+    end
+  end
   config.redis = { url: ENV['REDISTOGO_URL'],
   								size: 1,
   					 namespace: "openbid_#{ Rails.env }" }
