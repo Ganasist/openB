d55bdb5ab2587782c985fb38505a59d0b824c529
John Trichereau <johntrichereau@gmail.com>
2014-11-20 12:58:47 +0100

    MU to sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -16,7 +16,7 @@ Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
     if heroku
       p "[Sidekiq] Running on Heroku, autoscaler is used"
-      chain.add(Autoscaler::Sidekiq::Server, heroku, 60) # 60 seconds timeout
+	    chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuScaler.new
     else
       p "[Sidekiq] Running locally, so autoscaler isn't used"
     end
@@ -30,8 +30,8 @@ end
 Sidekiq.configure_client do |config|
 	if heroku
     config.client_middleware do |chain|
-      chain.add Autoscaler::Sidekiq::Client, 'default' => heroku
-    end
+	    chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuScaler.new
+	  end
   end
   config.redis = { url: ENV['REDISTOGO_URL'],
   								size: 1,
