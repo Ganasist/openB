eb4d8aff927dc82034dc97278ae827c616348d47
John Trichereau <johntrichereau@gmail.com>
2014-11-20 13:22:13 +0100

    MU

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,9 +8,9 @@ if ENV['HEROKU_APP']
   heroku = Autoscaler::HerokuScaler.new
 end
 
-Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
-  [user, password] == ['john', 'loislane']
-end
+# Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+#   [user, password] == ['john', 'loislane']
+# end
 
 Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
