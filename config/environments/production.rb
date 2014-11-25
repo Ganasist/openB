1c611e8ccbd0cc1d5153ec1dff00f85857799edd
John Trichereau <johntrichereau@gmail.com>
2014-11-25 13:41:05 +0100

    MU

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
     password: ENV['SENDGRID_PASSWORD']
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { host: 'staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'www.openbid.contractors' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
