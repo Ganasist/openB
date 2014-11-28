4195cd17f21f7c1d7cc87db79b5f5d627399f928
John Trichereau <johntrichereau@gmail.com>
2014-11-28 13:36:37 +0100

    MU

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { host: 'openbid-staging.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
