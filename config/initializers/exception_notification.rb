ef2a63ea325a7bd19934884841a0155f6113eacc
John Trichereau <johntrichereau@gmail.com>
2014-11-28 11:49:17 +0100

    MU

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,46 +1,46 @@
-require 'exception_notification/rails'
-require 'exception_notification/sidekiq'
+# require 'exception_notification/rails'
+# require 'exception_notification/sidekiq'
 
-ExceptionNotification.configure do |config|
-  # Ignore additional exception types.
-  # ActiveRecord::RecordNotFound, AbstractController::ActionNotFound and ActionController::RoutingError are already added.
-  # config.ignored_exceptions += %w{ActionView::TemplateError CustomError}
+# ExceptionNotification.configure do |config|
+#   # Ignore additional exception types.
+#   # ActiveRecord::RecordNotFound, AbstractController::ActionNotFound and ActionController::RoutingError are already added.
+#   # config.ignored_exceptions += %w{ActionView::TemplateError CustomError}
 
-  # Adds a condition to decide when an exception must be ignored or not.
-  # The ignore_if method can be invoked multiple times to add extra conditions.
-  config.ignore_if do |exception, options|
-    Rails.env.development?
-  end
+#   # Adds a condition to decide when an exception must be ignored or not.
+#   # The ignore_if method can be invoked multiple times to add extra conditions.
+#   config.ignore_if do |exception, options|
+#     Rails.env.development?
+#   end
 
-  # Notifiers =================================================================
+#   # Notifiers =================================================================
 
-  # Email notifier sends notifications by email.
-  config.add_notifier :email, {
-    email: {
-      email_prefix:         "[ERROR] ",
-      sender_address:       %{"Notifier" <noreply@openbid.com>},
-      exception_recipients: %w{ lance@.openbid.contractors admin@infinitory.com },
-      email_format: :html
-      }
-    }
+#   # Email notifier sends notifications by email.
+#   config.add_notifier :email, {
+#     email: {
+#       email_prefix:         "[ERROR] ",
+#       sender_address:       %{"Notifier" <noreply@openbid.com>},
+#       exception_recipients: %w{ lance@.openbid.contractors admin@infinitory.com },
+#       email_format: :html
+#       }
+#     }
 
-  # Campfire notifier sends notifications to your Campfire room. Requires 'tinder' gem.
-  # config.add_notifier :campfire, {
-  #   :subdomain => 'my_subdomain',
-  #   :token => 'my_token',
-  #   :room_name => 'my_room'
-  # }
+#   # Campfire notifier sends notifications to your Campfire room. Requires 'tinder' gem.
+#   # config.add_notifier :campfire, {
+#   #   :subdomain => 'my_subdomain',
+#   #   :token => 'my_token',
+#   #   :room_name => 'my_room'
+#   # }
 
-  # HipChat notifier sends notifications to your HipChat room. Requires 'hipchat' gem.
-  # config.add_notifier :hipchat, {
-  #   :api_token => 'my_token',
-  #   :room_name => 'my_room'
-  # }
+#   # HipChat notifier sends notifications to your HipChat room. Requires 'hipchat' gem.
+#   # config.add_notifier :hipchat, {
+#   #   :api_token => 'my_token',
+#   #   :room_name => 'my_room'
+#   # }
 
-  # Webhook notifier sends notifications over HTTP protocol. Requires 'httparty' gem.
-  # config.add_notifier :webhook, {
-  #   :url => 'http://example.com:5555/hubot/path',
-  #   :http_method => :post
-  # }
+#   # Webhook notifier sends notifications over HTTP protocol. Requires 'httparty' gem.
+#   # config.add_notifier :webhook, {
+#   #   :url => 'http://example.com:5555/hubot/path',
+#   #   :http_method => :post
+#   # }
 
-end
+# end
