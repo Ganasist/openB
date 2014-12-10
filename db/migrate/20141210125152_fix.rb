e9dfadae9088ecbde6f9a022279c7867b124b9d7
John Trichereau <johntrichereau@gmail.com>
2014-12-10 14:08:56 +0100

    MU

--- db/migrate/20141210125152_fix.rb
+++ db/migrate/20141210125152_fix.rb
@@ -3,12 +3,11 @@ class Fix < ActiveRecord::Migration
   	remove_column :jobs, :city, :string if column_exists?(:jobs, :city)
   	remove_column :jobs, :state, :string if column_exists?(:jobs, :state)
 
-	  unless  column_exists?(:jobs, :status) do 
-	  	add_column :jobs, :status, :integer, default: 0
-	    add_index :jobs, :status, name: 'index_statuses_on_searching',  where: 'status = 0'
-	    add_index :jobs, :status, name: 'index_statuses_on_in_progress', where: 'status = 1'
-	    add_index :jobs, :status, name: 'index_statuses_on_completed',where: 'status = 2'
-	    add_index :jobs, :status, name: 'index_statuses_on_canceled',where: 'status = 3'
-	  end
+	   
+  	add_column :jobs, :status, :integer, default: 0 unless  column_exists?(:jobs, :status)
+    add_index :jobs, :status, name: 'index_statuses_on_searching',  where: 'status = 0' unless  column_exists?(:jobs, :status)
+    add_index :jobs, :status, name: 'index_statuses_on_in_progress', where: 'status = 1' unless  column_exists?(:jobs, :status)
+    add_index :jobs, :status, name: 'index_statuses_on_completed',where: 'status = 2' unless  column_exists?(:jobs, :status)
+    add_index :jobs, :status, name: 'index_statuses_on_canceled',where: 'status = 3' unless  column_exists?(:jobs, :status)
   end
 end
