8d31ebef9016e027ca76db04c3b24cbf45a75075
John Trichereau <johntrichereau@gmail.com>
2014-12-10 14:17:56 +0100

    MU

--- db/migrate/20141210131207_clean_ratyrate_tables.rb
+++ db/migrate/20141210131207_clean_ratyrate_tables.rb
@@ -1,12 +1,12 @@
 class CleanRatyrateTables < ActiveRecord::Migration
   def change
-  	if table_exists?('average_caches')
+  	if table_exists?(:average_caches)
   		drop_table :average_caches
   	end
-  	if table_exists?('rates')
+  	if table_exists?(:rates)
   		drop_table :rates
   	end
-  	if table_exists?('rating_caches')
+  	if table_exists?(:rating_caches)
   		drop_table :rating_caches
   	end
   end
