720f8b825c22e31503ebb859af216f8f106cf612
John Trichereau <johntrichereau@gmail.com>
2014-12-29 11:14:50 +0100

    Update Welcome Email copy

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,15 +1,15 @@
 class WelcomeMailer < ApplicationMailer
-  
+
   def contractor_welcome(contractor)
     @contractor = contractor
     mail(to: @contractor.email,
-    subject: 'Welcome to Openbid! Find nearby jobs today.')
+    subject: 'OPENBID Registration Confirmation')
   end
 
   def user_welcome(user)
     @user = user
     mail(to: @user.email,
-    subject: 'Welcome to Openbid! Find local contractors today.')
+    subject: 'OPENBID Registration Confirmation')
   end
 
 end
