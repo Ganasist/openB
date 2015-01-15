5ec0716ca460d4f4a86695635fe2b0282872f407
John Trichereau <johntrichereau@gmail.com>
2015-01-15 14:14:43 +0100

    Change config.assets.initialize_on_precompile = false

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets do |config|
-	config.initialize_on_precompile = true
+	config.initialize_on_precompile = false
 	# config.paths << "#{ Rails.root }/vendor/assets"
   config.precompile += %w( spritemap.png spritemap@2x.png   )
 end
