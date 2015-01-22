e89acb0adb9d2b83e22561c7a1bd0178398d0006
John Trichereau <johntrichereau@gmail.com>
2015-01-22 17:11:24 +0100

    Add contractor feed to User show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,11 @@ class UsersController < ApplicationController
     end
     @jobs = @user.jobs.includes(:contractor, :uploads, :review).order(created_at: :desc).page(params[:jobs])
 
+    @contractor_feed = Contractor.includes(:upload)
+                                 .near(@user, @user.search_radius)
+                                 .relevant_categories(@user.categories)
+                                 .order(updated_at: :desc)
+
     @bids = @user.bids.includes(:job).order(updated_at: :desc).page(params[:bids]).per(28)
     @reviews = @user.reviews.order(created_at: :desc).page(params[:reviews]).per(5)
   end
