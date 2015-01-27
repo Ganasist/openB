aceb8ab5662839529040e19ea8f3fd6df0eb88a4
aborys <kh.ror301@gmail.com>
2015-01-27 15:16:17 +0100

    fix issue in schema file

--- db/schema.rb
+++ db/schema.rb
@@ -274,3 +274,4 @@ ActiveRecord::Schema.define(version: 20150126162509) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
+end
