1d6053addc6988081c34cbbb2c840b8696547196
John Trichereau <johntrichereau@gmail.com>
2015-01-30 15:15:30 +0100

    Fixes #58. Add lat/lon/distance/categories search to API

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,16 +1,22 @@
 class API::V1::SearchesController < API::V1::VersionController
   def show
     @search = Search.new
-    @search.latitude  = params[:latitude]
-    @search.longitude = params[:longitude]
-    @search.distance  = params[:distance].present? ? params[:distance] : 50
+    @search.latitude   = params[:latitude]
+    @search.longitude  = params[:longitude]
+    @search.categories = params[:categories]
+    @search.distance   = params[:distance].present? ? params[:distance] : 50
 
     if @search.valid? && (@search.latitude.present? && @search.longitude.present?)
       @jobs = Job.lat_lon_search(@search.latitude, @search.longitude, @search.distance).includes(:uploads)
       @contractors = Contractor.lat_lon_search(@search.latitude, @search.longitude, @search.distance).includes(:uploads)
+
+      if categories = params[:categories]
+        @jobs = @jobs.relevant_categories(@search.categories)
+        @contractors = @contractors.relevant_categories(@search.categories)
+      end
       render :show
     else
-      render json: { message: 'Invalid or incomplete search parameters' }, status: 422
+      render json: { message: @search.errors.full_messages.to_sentence }, status: 422
     end
   end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -3,9 +3,10 @@ class Search < ActiveRecord::Base
 	attr_accessor :distance
 	attr_accessor :latitude
 	attr_accessor :longitude
+	attr_accessor :categories
 
-	validates :zip_code, numericality: true, allow_blank: true
-	validates :distance, presence: true, numericality: { greater_than_or_equal_to:  1, less_than_or_equal_to:  999 }
+	# validates :zip_code, numericality: true, allow_blank: true
+	validates :distance, numericality: { greater_than_or_equal_to:  1, less_than_or_equal_to:  999 }
 	validates :latitude, numericality: { greater_than_or_equal_to:  -90, less_than_or_equal_to:  90 }, allow_blank: true
 	validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, allow_blank: true
 
