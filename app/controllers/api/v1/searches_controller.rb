aaf3364ea19c1792377639866b7287a58378fae2
John Trichereau <johntrichereau@gmail.com>
2015-02-03 09:58:35 +0100

    Fix up searches controller. Fixes 61

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,20 +1,33 @@
 class API::V1::SearchesController < API::V1::VersionController
   def create
-    @search = Search.new
-    @search.latitude   = params[:latitude]
-    @search.longitude  = params[:longitude]
-    @search.categories = params[:categories]
+    @search = Search.new(zip_code: params[:zip_code],
+                         latitude: params[:latitude],
+                        longitude: params[:longitude],
+                       categories: params[:categories])
     @search.distance   = params[:distance].present? ? params[:distance] : 50
 
-    if @search.valid? && (@search.latitude.present? && @search.longitude.present?)
-      @jobs = Job.lat_lon_search(@search.latitude, @search.longitude, @search.distance).includes(:uploads)
-      @contractors = Contractor.lat_lon_search(@search.latitude, @search.longitude, @search.distance).includes(:uploads)
+    if @search.valid?
+      if @search.latitude.present? && @search.longitude.present?
+        @jobs = Job.lat_lon_search(@search.latitude, @search.longitude, @search.distance).includes(:uploads)
+        @contractors = Contractor.lat_lon_search(@search.latitude, @search.longitude, @search.distance).includes(:uploads)
 
-      if categories = params[:categories]
-        @jobs = @jobs.relevant_categories(@search.categories)
-        @contractors = @contractors.relevant_categories(@search.categories)
+        if categories = params[:categories]
+          @jobs = @jobs.relevant_categories(@search.categories)
+          @contractors = @contractors.relevant_categories(@search.categories)
+        end
+        render :show
+      elsif @search.zip_code.present? && (Search.exists?(['zip_code = ?', @search.zip_code]) && !Rails.env.staging?)
+        @jobs = Job.zip_search(@search.zip_code, @search.distance).includes(:uploads)
+        @contractors = Contractor.zip_search(@search.zip_code, @search.distance).includes(:uploads)
+
+        if categories = params[:categories]
+          @jobs = @jobs.relevant_categories(@search.categories)
+          @contractors = @contractors.relevant_categories(@search.categories)
+        end
+        render :show
+      elsif @search.zip_code.present? && !Search.exists?(['zip_code = ?', @search.zip_code])
+        render json: { message: "Zip code not found."}, status: 402
       end
-      render :show
     else
       render json: { message: "Invalid search: #{ @search.errors.full_messages.to_sentence }" }, status: 422
     end
--- app/models/search.rb
+++ app/models/search.rb
@@ -5,10 +5,10 @@ class Search < ActiveRecord::Base
 	attr_accessor :longitude
 	attr_accessor :categories
 
-	validates :zip_code, numericality: true, allow_blank: true
+	validates :zip_code, numericality: true, presence: true, if: Proc.new { |s| s.latitude.blank? || s.longitude.blank? }
 	validates :distance, numericality: { greater_than_or_equal_to:  1, less_than_or_equal_to:  999 }, allow_blank: true
-	validates :latitude, numericality: { greater_than_or_equal_to:  -90, less_than_or_equal_to:  90 }, allow_blank: true
-	validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, allow_blank: true
+	validates :latitude, numericality: { greater_than_or_equal_to:  -90, less_than_or_equal_to:  90 }, presence: true, if: Proc.new { |s| s.zip_code.blank? }
+	validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, presence: true, if: Proc.new { |s| s.zip_code.blank? }
 
   def readonly?
 	  !new_record?
